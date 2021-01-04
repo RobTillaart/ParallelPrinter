e9fca0153170ea622a1f813619e3ba2c0f59abe8
Rob Tillaart <rob.tillaart@gmail.com>
2021-01-04 18:18:14 +0100

    Arduino ci (#2)
    * add Arduino-CI
    * add unit test
    * extended the interface to access internal variables.

--- ParallelPrinter.cpp
+++ ParallelPrinter.cpp
@@ -1,16 +1,19 @@
 //
 //    FILE: ParallelPrinter.cpp
 //  AUTHOR: Rob Tillaart
-// VERSION: 0.2.0
+// VERSION: 0.2.1
 // PURPOSE: parallel printer class that implements the Print interface
 //    DATE: 2013-09-30
 //     URL: https://github.com/RobTillaart/ParallelPrinter
 //
-// HISTORY
-// 0.1.0    2013-09-30 initial release
-// 0.2.0    2020-05-26 refactor, examples
+//  HISTORY
+//  0.1.0   2013-09-30  initial release
+//  0.2.0   2020-05-26  refactor, examples
+//  0.2.1   2020-01-04  arduino-CI + unit test
+
 
 #include "ParallelPrinter.h"
+
 
 ParallelPrinter::ParallelPrinter()
 {
@@ -35,29 +38,29 @@ ParallelPrinter::ParallelPrinter(uint8_t STROBE, uint8_t BUSY, uint8_t OOP, uint
     _pin[i] = p[i];
     pinMode(_pin[i], OUTPUT);
   }
+  setLineLength(80);
+  setPageLength(60);
+  reset();
 }
+
 
 void ParallelPrinter::begin(uint8_t lineLength, uint8_t pageLength)
 {
-  _pos = 0;
-  _lineNr = 0;
-  _pageNr = 0;
-  _tabSize = 2;
-  _lineFeed = 1;
-  _strobeDelay = 2000;
-  _printLineNumber = false;
-
-  // page size parameters.
-  _lineLength = lineLength;
-  _pageLength = pageLength;
+  setLineLength(lineLength);
+  setPageLength(pageLength);
+  reset();
 }
 
 
-void ParallelPrinter::setTabSize(uint8_t n)
+void ParallelPrinter::reset()
 {
-  // 2,4,6,8 allowed
-  _tabSize = (n > 8) ? 8 : n/2 * 2;
-  if (_tabSize < 2) _tabSize = 2;
+  _pos             = 0;
+  _lineNr          = 0;
+  _pageNr          = 0;
+  _tabSize         = 2;
+  _lineFeed        = 1;
+  _strobeDelay     = 2000;
+  _printLineNumber = false;
 }
 
 
